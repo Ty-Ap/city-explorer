e37f1a8b344f0bfb5ee43a88c930d52fb31e1396
Ty Aponte <aaustinsk8@aol.com>
2023-01-14 07:14:52 -0800

    built out functionality, edited README, incorporated more React/Bootstrap components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
+import Form from 'react-bootstrap/form';
+import Button from 'react-bootstrap/Button'
+import ListGroup from 'react-bootstrap/ListGroup';
+import Image from 'react-bootstrap/Image';
+import Alert from 'react-bootstrap/Alert';
+import Container from 'react-bootstrap/Container';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      cityData: [],
+      error: false,
+      errorMessage: '',
+    }
+  }
+  handleInput = (event) => {
+    this.setState({
+      city: event.target.value
+    })
+  }
+  getCityData = async (event) => {
+    event.preventDefault();
+
+    try {
+      let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.city}&format=json`
+
+      let cityDataFromAxios = await axios.get(url)
+
+
+      let mapImage= `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${cityDataFromAxios.data[0].lat},${cityDataFromAxios.data[0].lon}&zoom=10`;
+
+      this.setState({
+        cityData: cityDataFromAxios.data[0],
+        mapImage: mapImage,
+        error: false
+      })
+
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMessage: `${error.message}`
+      })
+    }
+  }
+  render() {
+    return (
+      <>
+        <h1>One Day the Only Butterflies Left Will Be in Your Chest</h1>
+
+        <Form onSubmit={this.getCityData}>
+          <Form.Group>
+            <Form.Label as='form-label'>City Name:</Form.Label>
+            <Form.Control type='text' placeholder='City Name' onInput={this.handleInput}></Form.Control>
+            <Button type='submit'>Explore!</Button>
+          </Form.Group>
+        </Form>
+               {
+                this.state.error
+                  ? <Alert variant="warning">{this.state.errorMessage}</Alert>
+                  : <Container>
+                    <ListGroup as='list-group'>
+                      <ListGroup.Item>City: {this.state.cityData.display_name}</ListGroup.Item>
+                      <ListGroup.Item>Latitude: {this.state.cityData.lat}</ListGroup.Item>
+                      <ListGroup.Item>Longitude: {this.state.cityData.lon}</ListGroup.Item>
+                    </ListGroup>
+                    <Image src={this.state.cityMap}></Image>
+                  </Container>
+              }
+      
+            </>
+          );
+        }
+      }
+
 
 export default App;
